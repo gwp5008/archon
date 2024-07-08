b62f4f899e3126d0e6577c1355aad6ae2add57c9
gwp5008 <gwp5008@yahoo.com>
2024-07-07 21:14:31 -0600

    first commit
    Archer can move.

---
+++ scenes/archer_movement.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+@onready var animations = $AnimationPlayer
+	
+func _physics_process(_delta):
+	velocity.x = Input.get_action_strength("walk_right") - Input.get_action_strength("walk_left")
+	velocity.y = Input.get_action_strength("walk_down") - Input.get_action_strength("walk_up")
+	velocity = velocity * 300
+	move_and_slide()
+	updateAnimation()
+	
+func updateAnimation():
+	if velocity.length() == 0:
+		animations.stop()
+		
+	var direction = ""
+	if velocity.x < 0 && velocity.y == 0: direction = "left"
+	elif velocity.x > 0 && velocity.y == 0: direction = "right"
+	elif velocity.y < 0 && velocity.x == 0: direction = "up"
+	elif velocity.y > 0 && velocity.x == 0: direction = "down"
+	elif velocity.y < 0 && velocity.x < 0: direction = "upleft"
+	elif velocity.y < 0 && velocity.x > 0: direction = "upright"
+	elif velocity.y > 0 && velocity.x < 0: direction = "downleft"
+	elif velocity.y > 0 && velocity.x > 0: direction = "downright"
+	
+	if (direction != ""):
+		animations.play("walk_" + direction)
+
